147037fa8d755056ba50912aa2ff1c3ec1b02952
Vladislav Baranov <wladiik@gmail.com>
2023-05-21 04:18:41 +0300

    v1.0

--- js/tabs.js
+++ js/tabs.js
@@ -1,25 +1,10 @@
 $(document).ready(function () {
-  $("#tab-1").on("click", function () {
-    $(".chrono__nav-item").removeClass("active");
-    $(".chrono__item").hide(0);
+  $("[data-tab]").on("click", function () {
+    const index = $(this).attr("data-tab");
+    $(this).siblings("[data-tab]").removeClass("active");
+    $(this).addClass("active");
 
-    $("#tab-1").addClass("active");
-    $("#tab-item-1").slideDown();
-  });
-
-  $("#tab-2").on("click", function () {
-    $(".chrono__nav-item").removeClass("active");
-    $(".chrono__item").hide(0);
-
-    $("#tab-2").addClass("active");
-    $("#tab-item-2").slideDown();
-  });
-
-  $("#tab-3").on("click", function () {
-    $(".chrono__nav-item").removeClass("active");
-    $(".chrono__item").hide(0);
-
-    $("#tab-3").addClass("active");
-    $("#tab-item-3").slideDown();
+    $(`[data-tabitem=${index}]`).siblings("[data-tabitem]").hide(0);
+    $(`[data-tabitem=${index}]`).slideDown();
   });
 });
